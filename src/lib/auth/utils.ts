2e2f47045e4b5fbc6d06c038fb82fea42a015fa5
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-07-23 13:15:37 -0300

    code challenge
    utils is now generating the code challenge

---
+++ src/lib/auth/utils.ts
@@ -0,0 +1,36 @@
+async function generateCodeVerifier() {
+    //code-verifier = 43*128unreserved
+   //unreserved = ALPHA / DIGIT / "-" / "." / "_" / "~"
+   //ALPHA = %x41-5A / %x61-7A
+   //DIGIT = %x30-39
+
+   const randomBytes = new Uint8Array(32);
+   crypto.getRandomValues(randomBytes);
+   let str = '';
+   for (let i = 0; i < randomBytes.length; i++) {
+    str += String.fromCharCode(randomBytes[i]);
+   }
+
+   const result = btoa(str)
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '');
+    return result;
+}
+
+async function generateCodeChallenge(codeVerifier: string){
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await crypto.subtle.digest('SHA-256', data);
+
+    return btoa(String.fromCharCode(...new Uint8Array(digest)))
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+}
+
+export async function getCodeChallenge(){
+    const verifier = await generateCodeVerifier();
+    const challenge = await generateCodeChallenge(verifier);
+    return challenge;
+}
