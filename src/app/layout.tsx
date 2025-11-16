dc36c968691634958b9bdd4c1e6c19cb65e95b36
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-11-16 20:03:58 -0300

    Adds google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/Header"
+import Header from "@/components/Header";
 import { AuthProvider } from "@/context/AuthContext";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "NexAni",
@@ -16,10 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="vsc-initialized">
-
         <AuthProvider>
           <Header />
           {children}
+          <GoogleAnalytics gaId="G-V2HJWNE2ZG" />
         </AuthProvider>
       </body>
     </html>
